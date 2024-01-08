425cee0a603f098b78be518370174f9c41ff9b24
Walter Ayala <walter.ayala@bcr.gob.sv>
2024-01-08 09:39:08 -0600

    Add Subcategories Migration

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -15,12 +15,16 @@
                         </a>
                     </div>
                 </div>
-                <div class="mb-3">
+                <form class="container"  action="">
+                    <div class="mb-3 d-flex align-items-center justify-content-center gap-2">
 
-                    <label for="search" class="form-label">Buscar</label>
-                    <input class="form-control" type="text" id="search">
-
-                </div>
+                        <label for="search" class="mb-2 text-gray-900 uppercase">Buscar</label>
+                        <input class="bg-gray-50 border border-gray-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-50 p-2.5" type="text" id="search">
+                        <button class="btn rounded uppercase fw-bold d-flex justify-content-between align-items-center w-auto" style="background-color: #111e60; color: #f2f2f2;">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </form>
                 <div class="container p-3">
                     <div class="row">
                         <div class="col-12">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
-/** @type {import('tailwindcss').Config} */
+@type {import('tailwindcss').Config}
 export default {
-    content: [
+     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/views/*.blade.php',
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-            backgroundColor: {
-                'bcr':'#111e60',
             },
         },
     },
